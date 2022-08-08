3d948e9ea33081b97aa799bb098402d3e42428c1
hasegawa2073 <hasetatsu2001@icloud.com>
2022-08-08 13:16:48 +0900

    Add: PHPの画面から商品を追加できるようにした

---
+++ insert-input.php
@@ -0,0 +1,6 @@
+<p>商品を追加します</p>
+<form action="insert-output.php" method="POST">
+  商品名<input type="text" name="name">
+  価格<input type="text" name="price">
+  <input type="submit" value="追加">
+</form>
