2d624b11eaab2beecc96de3917db783cc798bae6
hasegawa2073 <hasetatsu2001@icloud.com>
2022-08-08 12:08:35 +0900

    Add: データベースの情報を一覧表示した

---
+++ all2.php
@@ -0,0 +1,13 @@
+<?php
+
+$pdo = new PDO(
+    "mysql:host=localhost;dbname=shop;charset=utf8",
+    "staff",
+    "password"
+);
+
+foreach ($pdo->query("select * from product") as $row) {
+    echo "<p>$row[id]:$row[name]:$row[price]</p>";
+}
+
+?>
